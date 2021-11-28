5878b8bf2a20e43cda6b59f3722b6671c1016c28
world970511 <57740138+world970511@users.noreply.github.com>
2021-11-28 11:05:18 +0900

    모달 정리 및 이미지필드

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ef0mxnu)m%wog0hwta-5ahf0n@@7y#os)f8k8!sp5vu0wyjtli'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yhs04104.pythonanywhere.com']
 
 
 # Application definition
